78c04ce1dce4673601f2e8f2d170f4f5c0026d63
Aitor Iturrioz <riturrioz@gmail.com>
2021-05-21 17:40:53 +0200

    Decompress every received BLE message
    Signed-off-by: Aitor Iturrioz <riturrioz@gmail.com>

--- src/iombian_config.py
+++ src/iombian_config.py
@@ -10,27 +10,22 @@ class IoMBianConfig():
 
     def __init__(self, callback):
         self.callback = callback
-        self.num_params = 0
+        self.num_messages = 0
 
-    def start_config_process(self, num_params):
-        logger.info(f"Configuration process has started ({num_params} parameters)")
-        self.num_params = num_params
+    def start_config_process(self, num_messages):
+        logger.info(f"Configuration process has started ({num_messages} messages)")
+        self.num_messages = num_messages
         self.emit("start", [])
 
     def finish_config_process(self, config):
-        # Check if config contains 'num_params' keys
-        num_keys = len(config.keys())
-        if num_keys != self.num_params:
-            logger.error(f"The received 'config' doesn't match the expected number of parameters ({num_keys}/{self.num_params})")
-            return
         logger.info("Configuration process has finished")
-        self.num_params = 0
+        self.num_messages = 0
         self.emit("done", [])
         self.callback(config)
 
     def cancel_config_process(self):
         logger.warn("Configuration process has been cancelled")
-        self.num_params = 0
+        self.num_messages = 0
         self.emit("error", [])
 
 Emit.Patch(IoMBianConfig)
--- src/iombian_config_values_characteristic.py
+++ src/iombian_config_values_characteristic.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
 from pybleno import Characteristic
+import gzip
 import json
 import logging
 import threading
 
 logger = logging.getLogger(__name__)
+
+
+def deep_update(source, overrides):
+    for key, value in overrides.items():
+        if isinstance(value, dict) and value:
+            returned = deep_update(source.get(key, {}), value)
+            source[key] = returned
+        else:
+            source[key] = overrides[key]
+    return source
 
 
 class IoMBianConfigValuesCharacteristic(Characteristic):
@@ -21,19 +32,19 @@ class IoMBianConfigValuesCharacteristic(Characteristic):
         self.iombian_config.on("start", self.on_start)
         self.iombian_config.on("error", self.on_error)
         self.iombian_config.on("done", self.on_done)
-        self.num_params = 0
-        self.processed_num_params = 0
+        self.num_messages = 0
+        self.processed_num_messages = 0
         self.config = {}
         self.config_timeout_timer = None
 
     def on_start(self):
         logger.debug("on_start has been called")
-        self.num_params = self.iombian_config.num_params
+        self.num_messages = self.iombian_config.num_messages
 
     def on_error(self):
         logger.debug("on_error has been called")
-        self.num_params = 0
-        self.processed_num_params = 0
+        self.num_messages = 0
+        self.processed_num_messages = 0
         self.config = {}
         if self.config_timeout_timer:
             self.config_timeout_timer.cancel()
@@ -41,15 +52,15 @@ class IoMBianConfigValuesCharacteristic(Characteristic):
 
     def on_done(self):
         logger.debug("on_done has been called")
-        self.num_params = 0
-        self.processed_num_params = 0
+        self.num_messages = 0
+        self.processed_num_messages = 0
         self.config = {}
         if self.config_timeout_timer:
             self.config_timeout_timer.cancel()
             self.config_timeout_timer = None
 
     def onWriteRequest(self, data, offset, withoutResponse, callback):
-        if self.num_params == 0:
+        if self.num_messages == 0:
             logger.error("The configuration process has not been started, please, check the protocol")
             callback(Characteristic.RESULT_UNLIKELY_ERROR)
             return
@@ -58,17 +69,18 @@ class IoMBianConfigValuesCharacteristic(Characteristic):
         self.config_timeout_timer = threading.Timer(4.0, self.iombian_config.cancel_config_process)
         self.config_timeout_timer.start()
         try:
-            data_string = str(data, 'utf-8')
+            data_decompressed = gzip.decompress(data)
+            data_string = data_decompressed.decode()
             config = json.loads(data_string)
-        except (UnicodeDecodeError, json.decoder.JSONDecodeError) as e:
-            logger.error(f"A non valid JSON string has been received: {e}")
+        except (UnicodeDecodeError, json.decoder.JSONDecodeError, OSError) as e:
+            logger.error(f"A non valid compressed JSON string has been received: {e}")
             callback(Characteristic.RESULT_UNLIKELY_ERROR)
             self.iombian_config.cancel_config_process()
             return
-        self.config.update(config)
-        self.processed_num_params += 1
-        if int(self.processed_num_params) == int(self.num_params):
-            logger.debug("All expected parameters has been received")
+        self.config = deep_update(self.config, config)
+        self.processed_num_messages += 1
+        if int(self.processed_num_messages) == int(self.num_messages):
+            logger.debug("All expected messages has been received")
             if self.config_timeout_timer:
                 self.config_timeout_timer.cancel()
             self.iombian_config.finish_config_process(self.config)
